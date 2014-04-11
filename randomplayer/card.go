e6a3372b08d0f092544204d9f7b63259f61641b5
James Baxter <jebaxter@hotmail.co.uk>
2014-04-11 21:21:39 +0100

    Added randomplayer for testing

---
+++ randomplayer/card.go
@@ -0,0 +1,79 @@
+package main
+
+import "strings"
+
+type Card uint8
+
+const (
+	None = iota
+	Soldier
+	Clown
+	Knight
+	Priestess
+	Wizard
+	General
+	Minister
+	Princess
+)
+
+func (c Card) String() string {
+	switch c {
+	case Soldier: 
+		return "soldier"
+	case Clown: 
+		return "clown"
+	case Knight: 
+		return "knight"
+	case Priestess: 
+		return "priestess"
+	case Wizard: 
+		return "wizard"
+	case General: 
+		return "general"
+	case Minister: 
+		return "minister"
+	case Princess: 
+		return "princess"
+	default: 
+		return "<NONE>"
+	}
+}
+
+func ParseCard(str string) Card {
+	switch str[0] {
+	case 's', 'S':
+		if strings.EqualFold(str, "soldier") {
+			return Soldier
+		}
+	case 'c', 'C':
+		if strings.EqualFold(str, "clown") {
+			return Clown
+		}
+	case 'k', 'K':
+		if strings.EqualFold(str, "knight") {
+			return Knight
+		}
+	case 'p', 'P':
+		if strings.EqualFold(str, "priestess") {
+			return Priestess
+		}
+		if strings.EqualFold(str, "princess") {
+			return Princess
+		}
+	case 'w', 'W':
+		if strings.EqualFold(str, "wizard") {
+			return Wizard
+		}
+	case 'g', 'G':
+		if strings.EqualFold(str, "general") {
+			return General
+		}
+	case 'm', 'M':
+		if strings.EqualFold(str, "minister") {
+			return Minister
+		}
+	default: 
+		return None
+	}
+	return None
+}
---
+++ randomplayer/main.go
@@ -0,0 +1,117 @@
+package main
+
+import "fmt"
+import "os"
+import "strings"
+import "strconv"
+import "math/rand"
+import "time"
+import "io"
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	var hand = make([]Card, 0, 2)
+	var str string
+
+	var myNumber int
+	str = Recieve()
+	fmt.Sscanf(str, "ident %v", &myNumber)
+	fmt.Println("Random Player")
+
+	var totalPlayers int
+	str = Recieve()
+	fmt.Sscanf(str, "players %v", &totalPlayers)
+	var players = make([]int, totalPlayers)
+	for i, _ := range players {
+		players[i] = i
+	}
+	players = append(players[:myNumber], players[myNumber:]...)
+
+	var startPlayer int
+	str = Recieve()
+	fmt.Sscanf(str, "start %v", &startPlayer)
+
+	str = Recieve()
+	var card string
+	fmt.Sscanf(str, "draw %v", &card)
+	hand = append(hand, ParseCard(card))
+	
+	for {
+		str = Recieve()
+		var message string
+		var arg string
+		fmt.Sscan(str, &message, &arg)
+		
+		if strings.EqualFold(message, "player") {
+			if n, _ := strconv.ParseInt(arg, 10, 0); int(n) == myNumber {
+				//my turn!
+				//expect a draw message next
+				str = Recieve()
+				fmt.Sscanf(str, "draw %v", &arg)
+				hand = append(hand, ParseCard(arg))
+				
+				//pick a random card and play it
+				i := rand.Intn(len(hand))
+				c := hand[i]
+				hand = append(hand[:i], hand[i+1:]...)
+				switch c {
+				case Princess, Minister, Priestess:
+					//cards taking no arguments
+					fmt.Println("play", c)
+				case General, Wizard, Knight, Clown:
+					//cards requiring a target
+					i = rand.Intn(len(players))
+					fmt.Println("play", c, players[i])
+				case Soldier:
+					//card requiring a target and a query
+					i = rand.Intn(len(players))
+					q := Card(rand.Intn(8)+1)
+					fmt.Println("play", c, players[i], q)
+				default:
+					//something must have gone wrong
+					fmt.Println("forfeit")
+				}
+			}
+		} else if strings.EqualFold(message, "draw") {
+			//add the specified card to hand
+			hand = append(hand, ParseCard(arg))
+		} else if strings.EqualFold(message, "swap") {
+			//swap the first card in hand for the specified card 
+			c := ParseCard(arg)
+			hand[0] = c
+		} else if strings.EqualFold(message, "discard") {
+			//remove the specified card from hand
+			c := ParseCard(arg)
+			for i, v := range hand {
+				if v == c {
+					hand = append(hand[:i], hand[i+1:]...)
+				}
+			}
+		} else if strings.EqualFold(message, "out") {
+			//can't target a player that's out so remove them from the players list
+			n, _ := strconv.ParseInt(arg, 10, 0)
+			for i, v := range players {
+				if v == int(n) {
+					players = append(players[:i], players[i+1:]...)
+				}
+			}
+		}
+		//all other messages require no handling
+	}
+}
+
+func Recieve() string {
+	buf := make([]byte, 1)
+	str := make([]byte, 0, 20)
+	n, err := os.Stdin.Read(buf)
+	for ; buf[0] != '\n'; n, err = os.Stdin.Read(buf) {
+		if err == nil && n > 0 {
+			str = append(str, buf[0])
+		} else if err != io.EOF {
+			panic(err)
+		}
+	}
+	
+	return string(str)
+}
